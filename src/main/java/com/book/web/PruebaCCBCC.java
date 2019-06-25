fe7001a70a565ca58476b7b95227ce1c5be7a05f
avbravo <avbravo@avbravo>
2019-06-25 13:44:35 -0500

    multiples emails

--- src/main/java/com/book/web/PruebaCCBCC.java
+++ src/main/java/com/book/web/PruebaCCBCC.java
@@ -8,7 +8,9 @@ package com.book.web;
 import com.avbravo.jmoordbutils.JsfUtil;
 import com.avbravo.jmoordbutils.email.ManagerEmail;
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -16,21 +18,51 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 /**
+ * Envia correos con copias (cc, bcc y varios destinatarios)
  *
  * @author avbravo
  */
 @Named
 @ViewScoped
-public class Prueba implements Serializable {
+public class PruebaCCBCC implements Serializable {
 
-     String emailemisor="";
-    String passwordemailemisor="";
-    String emailreceptor="";
+    String emailemisor = "";
+    String passwordemailemisor = "";
+    String[] to = {"unoo@gmail.com", "dos@gmail.com"}; // list of recipient email addresses
+    String[] cc = {"tres@gmail.com"};
+    String[] bcc = {"cuatro@gmail.com", "cinco@gmail.com"};
+
+    public String getEmailemisor() {
+        return emailemisor;
+    }
+
+    public void setEmailemisor(String emailemisor) {
+        this.emailemisor = emailemisor;
+    }
+
+    public String getPasswordemailemisor() {
+        return passwordemailemisor;
+    }
+
+    public void setPasswordemailemisor(String passwordemailemisor) {
+        this.passwordemailemisor = passwordemailemisor;
+    }
+
+    public Integer getContadoremail() {
+        return contadoremail;
+    }
+
+    public void setContadoremail(Integer contadoremail) {
+        this.contadoremail = contadoremail;
+    }
+
+    
+    
+    
+    
     ManagerEmail managerEmail = new ManagerEmail();
-  
-    Integer contadoremail = 0;
 
-  
+    Integer contadoremail = 0;
 
     public void enviar() {
         try {
@@ -61,16 +93,19 @@ public class Prueba implements Serializable {
         CompletableFuture<String> completableFuture
                 = new CompletableFuture<>();
 
-        Executors.newCachedThreadPool().submit(() -> {
-            System.out.println("........ Voy a enviar el email");
-            Date date = new Date();
-            String mensaje="prueba a " + date.toString();
-            managerEmail.sendOutlook(emailreceptor, "{Async}:" + contadoremail, mensaje, emailemisor, passwordemailemisor);
-            System.out.println("------------->Email enviado a "+emailreceptor);
-//              Thread.sleep(2500);
-            completableFuture.complete("Hello"); 
+        Executors.newCachedThreadPool().submit(new Callable<Object>() {
+            @Override
+            public Object call() throws Exception {
+                System.out.println("........ Voy a enviar el email");
+                Date date = new Date();
 
-            return null;
+                managerEmail.sendOutlook(to, cc, bcc, "{Async}:" + contadoremail, "prueba a " + date.toString(), emailemisor, passwordemailemisor);
+                System.out.println(".......... Emails enviados con copias: " + Arrays.toString(to) + " cc " + Arrays.toString(cc) + " bcc " + Arrays.toString(bcc));
+//              Thread.sleep(2500);
+                completableFuture.complete("Hello");
+
+                return null;
+            }
         });
 
         return completableFuture;
